7255ed8d80d8bafee99a17735c310979cef8c768
Shonta Wilson <swilson9894@gmail.com>
2020-08-12 19:33:30 -0500

    adding newmap

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ $.ajax({
   }).then(function(data) {
     console.log(data);
   })
+
+
+
+  function initMap(){
+    var option = {
+      zoom: 14,
+      center: {lat: 41.8781136, lng:-87.6297982}
+    }
+
+    var map = new 
+    google.maps.Map(document.getElementById('map'), option);
+
+  }
+  initMap() 
+

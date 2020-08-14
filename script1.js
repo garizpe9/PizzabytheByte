00a2b45d336bd62b4f28e7e0d1ead8132979b3d9
Frida <velia.arizpe@gmail.com>
2020-08-13 21:58:54 -0500

    updated function name and removed tbody

--- script1.js
+++ script1.js
@@ -4,7 +4,7 @@ var apikey =
 var clientID = "vUaKKoVHSe-3_ow_x9Y19A";
 var queryURL = "https://api.yelp.com/v3/businesses/search";
 
-$("#nav_search").on("click", somurl);
+$("#nav_search").on("click", sumurl);
 
 function sumurl() {
   var myurl2 =
@@ -23,7 +23,7 @@ function sumurl() {
     method: "GET",
     dataType: "json",
     success: function (data) {
-      $("#pizzarating").append("<tbody>")
+      //$("#pizzarating").append("<tbody>")
       for (var i = 0; i < 10; i++) {
         $("#pizzarating").append("<tr>").append("<td id='" + i + "'>")
         $("#" + (i)).html(data.businesses[i].name + ", " + data.businesses[i].price + ", " + data.businesses[i].rating + ", " + data.businesses[i].coordinates.latitude + ", " + data.businesses[i].coordinates.longitude)

fdfe89b4b1037958afc1ca503bc592c4ec526169
Shonta Wilson <swilson9894@gmail.com>
2020-08-15 14:23:50 -0500

    workingmap

--- script1.js
+++ script1.js
@@ -47,11 +47,47 @@ function sumurl() {
     },
   });
 }
-function initMap() {
-  var option = {
-    zoom: 14,
-    center: { lat: 41.8781136, lng: -87.6297982 },
-  };
+$.ajax({
+  url: "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=41.8781136%2C-87.6297982&radius=5000&keyword=Pizza&key=AIzaSyCWWS-XgxQJjyRBc1Yyi7wAxaep1RJGN4o",
+  headers: {
+    Authorization:
+    "AIzaSyCWWS-XgxQJjyRBc1Yyi7wAxaep1RJGN4o",
+  },
+  method: "GET",
+  dataType: "json",
+  success: function (data) {console.log(data)
+  console.log(data.results)
+  var retrieved =[]
 
-  var map = new google.maps.Map(document.getElementById("map"), option);
-}
+  for (var i = 0; i <data.results.length; i++) {
+retrieved.push({lat:data.results[i].geometry.location.lat,lng:data.results[i].geometry.location.lng,name:data.results[i].name})
+  }
+    
+  console.log(retrieved)
+  initMap(retrieved)
+    
+  }}) 
+
+
+
+
+
+    
+    
+    function initMap(value) {
+      var myLatLng = {lat: 41.8781136, lng: -87.6297982};
+    
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 14,
+        center: myLatLng
+      });
+    
+      for (var i = 0; i < value.length; i++){
+      var marker = new google.maps.Marker({
+        position: {lat: value[i].lat, lng: value[i].lng}, 
+        map: map,
+        title: value[i].name
+      });
+    }
+
+  }
